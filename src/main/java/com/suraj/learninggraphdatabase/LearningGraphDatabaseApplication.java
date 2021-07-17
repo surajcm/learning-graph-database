a7b592fe04280691302350c2002d213da548b6cd
Suraj Muraleedharan <v-smuraleedharan@expediagroup.com>
2021-07-17 08:23:01 +0530

    first commit

---
+++ src/main/java/com/suraj/learninggraphdatabase/LearningGraphDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.suraj.learninggraphdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningGraphDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningGraphDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suraj/learninggraphdatabase/LearningGraphDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suraj.learninggraphdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningGraphDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

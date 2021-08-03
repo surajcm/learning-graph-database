72191be9ba7afaf50be82ef7c188ce94e419a174
Suraj Muraleedharan <v-smuraleedharan@expediagroup.com>
2021-08-03 09:15:50 +0530

    added some basic pojos to test with and planning to try jgrapht

---
+++ src/main/java/com/suraj/learninggraphdatabase/common/model/Metadata.java
@@ -0,0 +1,22 @@
+package com.suraj.learninggraphdatabase.common.model;
+
+public class Metadata {
+    private Long parent;
+    private Long[] children;
+
+    public Long getParent() {
+        return parent;
+    }
+
+    public void setParent(Long parent) {
+        this.parent = parent;
+    }
+
+    public Long[] getChildren() {
+        return children;
+    }
+
+    public void setChildren(Long[] children) {
+        this.children = children;
+    }
+}
---
+++ src/main/java/com/suraj/learninggraphdatabase/common/model/TGModel.java
@@ -0,0 +1,31 @@
+package com.suraj.learninggraphdatabase.common.model;
+
+public class TGModel {
+    private boolean isDGEnabled;
+    private String destinationType;
+    private Metadata metadata;
+
+    public boolean isDGEnabled() {
+        return isDGEnabled;
+    }
+
+    public void setDGEnabled(boolean DGEnabled) {
+        isDGEnabled = DGEnabled;
+    }
+
+    public String getDestinationType() {
+        return destinationType;
+    }
+
+    public void setDestinationType(String destinationType) {
+        this.destinationType = destinationType;
+    }
+
+    public Metadata getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(Metadata metadata) {
+        this.metadata = metadata;
+    }
+}
